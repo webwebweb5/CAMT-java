43a0faf920d216f75bf19ffb23c8248927fd01ee
webwebweb5 <68845415+webwebweb5@users.noreply.github.com>
2020-09-13 14:35:57 +0700

    Create inputTemperature.java

---
+++ inputTemperature.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class temperature {
+
+	public static void main(String[] args) {
+		
+		//Scanner input = new Scanner(System.in);
+		
+		//°F to °C
+		/*System.out.print("Enter temperature °F : ");
+		double f = input.nextDouble();
+		
+		double c = (f-32)*0.56;
+		
+		System.out.println(f + " °F = " + c + " °C");
+		*/
+		
+		//°C to °F
+		/*System.out.print("Enter temperature °C : ");
+		double c = input.nextDouble();
+		
+		double f = (9*c)/5+32;
+		
+		System.out.println(c + " °C = " + f + " °F");
+		*/
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter temperature °C or °F : ");
+		char choice = input.next().charAt(0);
+		
+		
+		if (choice == 'c' || choice == 'C') {
+			System.out.print("Enter temperature °C : ");
+			double cel = input.nextDouble();
+			double f = (9*cel)/5+32;
+			System.out.println(cel + " °C = " + f + " °F");
+		} else if (choice == 'f' || choice == 'F') {
+			System.out.print("Enter temperature °F : ");
+			double f = input.nextDouble();
+			double c = (f-32)*0.56;
+			System.out.println(f + " °F = " + c + " °C");
+		}
+		
+	}
+
+}
