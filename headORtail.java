6fed524208ae6f6562087779a800cabb415065af
webwebweb5 <68845415+webwebweb5@users.noreply.github.com>
2020-09-13 13:46:07 +0700

    Update headORtail.java

--- headORtail.java
+++ headORtail.java
@@ -1,17 +1,31 @@
-import java.util.Random;
+//import java.util.Random;
+import java.util.Scanner;
 
 public class headORtail {
 
 	public static void main(String[] args) {
 		
-		Random rand = new Random();
+		/*Random rand = new Random();
 		int random = rand.nextInt(2);
 		
 		if (random == 1) {
 			System.out.print("Hoorayyyy! You are win!");
 		} else {
 			System.out.print("Boooo! You are lose!");
+		}*/
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter number([0] = head, [1] = tail) : ");
+		int num = input.nextInt();
+		
+		num = (int)(Math.random() * 2);
+		
+		if (num == 1) {
+			System.out.print("Hoorayyyy! You are win! correct answer is " + num);
+		} else {
+			System.out.print("Boooo! You are lose! correct answer is " + num);
 		}
+		
 	}
 
 }
